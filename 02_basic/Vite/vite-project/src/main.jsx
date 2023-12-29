6bea364df3832c82f73f00469b1acb8e9ac7a4ea
Rohit <singhrohit51967@gmail.com>
2023-12-29 15:01:29 +0530

    conversion of component function to tree object

--- 02_basic/Vite/vite-project/src/main.jsx
+++ 02_basic/Vite/vite-project/src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// element is not a function. It's a variable that holds a JSX element.
+const element = (
+  <a href='https://google.com' target='_blank'>Click me to visit google</a>
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <App />
+  // App()
+  element
 )
