0c0f6a1f8dfb50cd080f1f7c060cd1e4a2c8be3e
Rohit <singhrohit51967@gmail.com>
2023-12-30 14:18:43 +0530

    Interview Question

--- 04_hooks/Counter(useState)/CounterProject/src/App.jsx
+++ 04_hooks/Counter(useState)/CounterProject/src/App.jsx
@@ -5,7 +5,18 @@ function App() {
   const [counter, setCounter] = useState(0)
 
   function increase() {
-    setCounter(counter + 1);
+    //ye sara ek hi batch(similar work) me jyega islye counter ek hi bar incerease hoga
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+
+    //Ye ek batch me nhi jyega as it depend on previouschanges isliye counter increase by 4
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+
   }
   function decrease() {
     if (counter > 0)
