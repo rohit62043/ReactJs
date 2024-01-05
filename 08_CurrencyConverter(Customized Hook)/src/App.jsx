62a713e37b5ffd4c2ef39d7abaccc89981407955
Rohit <singhrohit51967@gmail.com>
2024-01-05 20:17:01 +0530

    component

--- 08_CurrencyConverter(Customized Hook)/src/App.jsx
+++ 08_CurrencyConverter(Customized Hook)/src/App.jsx
@@ -1,11 +1,9 @@
-
+import InputBox from "./components/Input"
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <InputBox />
   )
 }
 
