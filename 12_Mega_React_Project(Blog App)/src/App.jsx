ab9588189986eab4e3c29be18223e7a20e096fc6
Rohit <singhrohit51967@gmail.com>
2024-04-14 16:12:58 +0530

    Completed

--- 12_Mega_React_Project(Blog App)/src/App.jsx
+++ 12_Mega_React_Project(Blog App)/src/App.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import conf from './conf/conf'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './component/index'
+
 function App() {
-  console.log(conf)
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      } else {
+        dispatch(logout())
+      }
+
+    }).finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div>
+        <Header />
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
